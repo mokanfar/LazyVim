1a574d3118996e6a696c5703cb0340244b33a379
mokanfar <dannyz1984@gmail.com>
2024-02-21 21:47:40 -0800

    Remove license and add DS_STORE to git ignore

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -5,8 +5,6 @@ return {
   -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    -- calling `setup` is optional for customization
-    require("fzf-lua").setup({})
   end
 }
 }
