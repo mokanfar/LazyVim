24e2a47b3f43695e463a5f5f48b239e2c218242e
mokanfar <dannyz1984@gmail.com>
2024-03-05 21:48:36 -0800

    Add Dir Telescope extension

---
+++ lua/plugins/dir-telescope.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "princejoogie/dir-telescope.nvim",
+    -- telescope.nvim is a required dependency
+    requires = { "nvim-telescope/telescope.nvim" },
+    keys = {
+      {
+        "<leader>jd",
+        function()
+            require("telescope").extensions.dir.live_grep({no_ignore = true, hidden = true})
+        end,
+        desc = "Live Grep inside specific dir",
+      },
+    },
+    config = function()
+      require("dir-telescope").setup({
+        -- these are the default options set
+        hidden = false,
+        no_ignore = false,
+        show_preview = true,
+      })
+    end,
+  },
+}
