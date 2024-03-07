5c2421ef34bffe0f19aa316f9a0422a06a0185b4
mokanfar <dannyz1984@gmail.com>
2024-03-06 22:18:05 -0800

    Telescope modifications

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,9 +6,37 @@ return {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
       },
-      "nvim-telescope/telescope-file-browser.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      "zane-/cder.nvim",
+      "nvim-lua/plenary.nvim",
     },
+
     keys = {
+      {
+        "<leader>fg",
+        function()
+          require("telescope").extensions.live_grep_args.live_grep_args({
+            additional_args = function(args)
+              return vim.list_extend(args, { "--hidden", "--no-ignore" })
+            end,
+          })
+        end,
+        desc = "Live Grep (root/dynamic)",
+      },
+      {
+        "<leader>:",
+        function()
+          require("telescope.builtin").commands()
+        end,
+        desc = "All Commands",
+      },
+      {
+        "<leader>;",
+        function()
+          require("telescope.builtin").command_history()
+        end,
+        desc = "Command history",
+      },
       {
         "<leader>jp",
         function()
@@ -17,49 +45,6 @@ return {
           })
         end,
         desc = "Find Plugin File",
-      },
-      {
-        "<leader>jf",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.find_files({
-            no_ignore = false,
-            hidden = true,
-          })
-        end,
-        desc = "Lists files in your current working directory, respects .gitignore",
-      },
-      {
-        "<leader>jr",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.live_grep()
-        end,
-        desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore",
-      },
-      {
-        "\\\\",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.buffers()
-        end,
-        desc = "Lists open buffers",
-      },
-      {
-        "<leader>jt",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.help_tags()
-        end,
-        desc = "Lists available help tags and opens a new window with the relevant help info on <cr>",
-      },
-      {
-        "<leader>j;",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.resume()
-        end,
-        desc = "Resume the previous telescope picker",
       },
       {
         "<leader>je",
@@ -77,43 +62,27 @@ return {
         end,
         desc = "Lists Function names, variables, from Treesitter",
       },
-      {
-        "<leader>jF",
-        function()
-          local telescope = require("telescope")
-
-          local function telescope_buffer_dir()
-            return vim.fn.expand("%:p:h")
-          end
-
-          telescope.extensions.file_browser.file_browser({
-            path = "%:p:h",
-            cwd = telescope_buffer_dir(),
-            respect_gitignore = false,
-            hidden = true,
-            grouped = true,
-            previewer = false,
-            initial_mode = "normal",
-            layout_config = { height = 40 },
-          })
-        end,
-        desc = "Open File Browser with the path of the current buffer",
-      },
     },
     config = function(_, opts)
       local telescope = require("telescope")
-      local actions = require("telescope.actions")
-      local fb_actions = require("telescope").extensions.file_browser.actions
-
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-n>"] = function(picker)
+                require("telescope.actions").send_selected_to_qflist(picker)
+                vim.cmd("copen")
+              end,
+            },
+          },
+        },
+      })
       opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
         wrap_results = true,
         layout_strategy = "horizontal",
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 0,
-        mappings = {
-          n = {},
-        },
       })
       opts.pickers = {
         diagnostics = {
@@ -124,39 +93,18 @@ return {
           },
         },
       }
+
       opts.extensions = {
-        file_browser = {
-          theme = "dropdown",
-          -- disables netrw and use telescope-file-browser in its place
-          hijack_netrw = true,
-          mappings = {
-            -- your custom insert mode mappings
-            ["n"] = {
-              -- your custom normal mode mappings
-              ["N"] = fb_actions.create,
-              ["h"] = fb_actions.goto_parent_dir,
-              ["/"] = function()
-                vim.cmd("startinsert")
-              end,
-              ["<C-u>"] = function(prompt_bufnr)
-                for i = 1, 10 do
-                  actions.move_selection_previous(prompt_bufnr)
-                end
-              end,
-              ["<C-d>"] = function(prompt_bufnr)
-                for i = 1, 10 do
-                  actions.move_selection_next(prompt_bufnr)
-                end
-              end,
-              ["<PageUp>"] = actions.preview_scrolling_up,
-              ["<PageDown>"] = actions.preview_scrolling_down,
-            },
-          },
+        cder = {
+          dir_command = { "fd", "--type=d", "." },
         },
+        -- live_grep_args = {
+        --       auto_quoting = false
+        -- },
       }
       telescope.setup(opts)
       require("telescope").load_extension("fzf")
-      require("telescope").load_extension("file_browser")
+      require("telescope").load_extension("cder")
     end,
   },
 }
