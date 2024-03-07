a2ccd99e526d8730a2524bcbfc6941db2b28ce0d
mokanfar <dannyz1984@gmail.com>
2024-03-06 23:02:47 -0800

    Keymap changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,34 +1,55 @@
-vim.api.nvim_set_keymap("n", "<C-\\>", ":FzfLua<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("n", "<C-r>", ":FzfLua command_history<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-r>", ":FzfLua oldfiles<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-p>", ":FzfLua buffers<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("n", "<C-j>", ":FzfLua jumps<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-r>",
+  ":lua require('telescope.builtin').oldfiles()<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "i",
+  "<C-f>",
+  ":lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-f>",
+  ":lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-p>",
+  ":lua require('telescope.builtin').buffers()<CR>",
+  { noremap = true, silent = true }
+)
 vim.api.nvim_set_keymap("n", "<C-y>", ":redo<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-n>", ":enew<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader><space>",
+  ':lua require("telescope.builtin").resume()<CR>',
+  { noremap = true, silent = true }
+)
 vim.api.nvim_set_keymap("n", "<C-w>", ":bd<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "o", "5o<Esc>2k", { noremap = true  })
+vim.api.nvim_set_keymap("i", "<C-l>", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "o", "3o<Esc>1k", { noremap = true })
 -- vim.api.nvim_del_keymap("n", "[")
 -- vim.api.nvim_del_keymap("n", "]")
 -- vim.api.nvim_set_keymap("n", "[", ":bprev<CR>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("n", "]", ":bnext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-l>", "<Esc>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-f>", ":FzfLua blines<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-f>", "<Esc>:FzfLua blines<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "T", "<cmd>lua require('arrow.persist').toggle()<CR><cmd>echo 'File Tagged'<CR>", { desc = "ToggLe Arrow Tag" })
+vim.api.nvim_set_keymap(
+  "n",
+  "T",
+  "<cmd>lua require('arrow.persist').toggle()<CR><cmd>echo 'File Tagged'<CR>",
+  { desc = "ToggLe Arrow Tag" }
+)
 vim.api.nvim_set_keymap("n", "H", "<cmd>lua require('arrow.persist').previous()<CR>", { desc = "Pevious Arrow Tag" })
 vim.api.nvim_set_keymap("n", "L", "<cmd>lua require('arrow.persist').next()<CR>", { desc = "Next Arrow Tag" })
-vim.api.nvim_set_keymap("n", "<C-u>", "<cmd>lua require('arrow.persist').next()<CR>", { desc = "Next Arrow Tag" })
 vim.api.nvim_set_keymap(
   "n",
   "<leader>fg",
   "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
   { desc = "Live Grep" }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<C-t>",
-  "<cmd>lua require('arrow.persist').toggle()<CR>",
-  { desc = "Toggle Tag Curr Buff" }
 )
 
 function FormatFunction()
@@ -37,10 +58,8 @@ function FormatFunction()
     range = {
       ["start"] = vim.api.nvim_buf_get_mark(0, "<"),
       ["end"] = vim.api.nvim_buf_get_mark(0, ">"),
-    }
+    },
   })
 end
 
-vim.api.nvim_set_keymap("v", "<leader>cf", "<Esc><cmd>lua FormatFunction()<CR>", {noremap = true})
-
-
+vim.api.nvim_set_keymap("v", "<leader>cf", "<Esc><cmd>lua FormatFunction()<CR>", { noremap = true })
