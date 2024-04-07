6743d74c8ffc8573bd3e2e8187b6e95da1ee7974
mokanfar <dannyz1984@gmail.com>
2024-04-06 21:37:48 -0700

    Add find replace handy funcs

--- lua/plugins/cmd.lua
+++ lua/plugins/cmd.lua
@@ -8,4 +8,41 @@ vim.api.nvim_create_user_command(
   end,
   {}
 )
+vim.api.nvim_create_user_command(
+  'FindReplace',
+  function()
+    local search_pattern = vim.fn.input('Enter search pattern: ')
+    if search_pattern ~= "" then
+      local substitution_pattern = vim.fn.input('Enter substitution pattern: ')
+      if substitution_pattern ~= "" then
+        local escaped_search_pattern = search_pattern:gsub('/', '\\/')
+        local escaped_substitution_pattern = substitution_pattern:gsub('/', '\\/')
+        vim.cmd(':%s/' .. escaped_search_pattern .. '/' .. escaped_substitution_pattern .. '/g')
+      end
+    end
+  end,
+  {}
+)
+vim.api.nvim_create_user_command(
+  'ReplaceLinesWithPattern',
+  function()
+    local pattern = vim.fn.input('Replace newlines with: ')
+    if pattern ~= "" then
+      local escaped_pattern = pattern:gsub('([&\\%$%-%.%*%[%]%(%)]+)', '\\%1')
+      vim.cmd(string.format("%%s/\\n/%s/g", escaped_pattern))
+    end
+  end,
+  {}
+)
+vim.api.nvim_create_user_command(
+  'ReplacePatternWithNewLine',
+  function()
+    local pattern = vim.fn.input('Enter Pattern: ')
+    if pattern ~= "" then
+      local escaped_pattern = pattern:gsub('/', '\\/')
+      vim.cmd(':%s/' .. escaped_pattern .. '/\\r/g')
+    end
+  end,
+  {}
+)
 return {}
