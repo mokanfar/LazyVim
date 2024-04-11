e22006f6984ce550ad86abc1292f77fef3e09287
mokanfar <dannyz1984@gmail.com>
2024-04-10 17:58:11 -0700

    Add live_grep CMD no-ignore flag

--- lua/plugins/cmd.lua
+++ lua/plugins/cmd.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_user_command(
   'LiveGrepVendorMagento',
   function()
     require("telescope.builtin").live_grep({
-      additional_args = { "-j1", "-g", "!*test*", "-g", "!*Test*", "--max-count=1" },
+      additional_args = { "-j1", "--no-ignore", "-g", "!*test*", "-g", "!*Test*", "--max-count=1" },
       search_dirs = { "./vendor/magento" }
     })
   end,
