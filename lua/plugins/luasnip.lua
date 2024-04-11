ef2aeb4c5e733cd5bd8e852a8692e9ab83e1f7c4
mokanfar <dannyz1984@gmail.com>
2024-04-10 17:49:55 -0700

    Lua Snip CMP conflict change keymaps

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,49 @@
+return {
+  "L3MON4D3/LuaSnip",
+  dependencies = {
+    {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+  },
+  opts = function()
+    local types = require "luasnip.util.types"
+    return {
+      history = true,
+      delete_check_events = "TextChanged",
+
+      -- Display a cursor-like placeholder in unvisited nodes of the snippet.
+      ext_opts = {
+        [types.insertNode] = {
+          unvisited = {
+            virt_text = { { "|", "Conceal" } },
+            -- virt_text_pos = "inline",
+          },
+        },
+        [types.exitNode] = {
+          unvisited = {
+            virt_text = { { "|", "Conceal" } },
+            -- virt_text_pos = "inline",
+          },
+        },
+      },
+    }
+  end,
+  -- stylua: ignore
+  keys = {
+    {
+      "<C-e>",
+      function()
+        return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<C-e>"
+      end,
+      expr = true,
+      remap = true,
+      silent = true,
+      mode = "i",
+    },
+    { "<C-e>", function() require("luasnip").jump(1) end,  mode = "s" },
+    { "<C-d>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+  }
+}
